aa3225ba3fcff2af0824a14724a498f497f483ed
BidlerShen <mrmartinirocks@gmail.com>
2020-10-03 15:39:10 +0930

    Rejigged Order
    Also removed the echo newlines

--- setup.sh
+++ setup.sh
@@ -13,67 +13,67 @@ BLU='\033[0;36m'
 NC='\033[0m'
 
 #Run the theme if applicable
-echo -e "\n${BLU}ezClaps${NC} - ${AMB}Updating theme...${NC}\n"
+echo -e "${BLU}ezClaps${NC} - ${AMB}Updating theme...${NC}"
 VER=$(lsb_release -r | awk '{print $2}')
 if [[ -d "modules/themes/$VER" ]]; then
         bash modules/themes/$VER/install.sh
-        echo -e "\n${BLU}ezClaps${NC} - ${GRN}Theme updated!${NC}\n"
+        echo -e "${BLU}ezClaps${NC} - ${GRN}Theme updated!${NC}"
 else
-        echo -e "\n${BLU}ezClaps${NC} - ${RED}No theme available!${NC}\n"
+        echo -e "${BLU}ezClaps${NC} - ${RED}No theme available!${NC}"
 fi
 
 #Set timezone to UTC
-echo -e "\n${BLU}ezClaps${NC} - ${AMB}Updating timezone...${NC}\n"
+echo -e "${BLU}ezClaps${NC} - ${AMB}Updating timezone...${NC}"
 sudo timedatectl set-timezone UTC
-echo -e "\n${BLU}ezClaps${NC} - ${GRN}Timezone updated to UTC!${NC}\n"
+echo -e "${BLU}ezClaps${NC} - ${GRN}Timezone updated to UTC!${NC}"
 
 #Install Python3 & PIP3
-echo -e "\n${BLU}ezClaps${NC} - ${AMB}Installing Python3 & PIP3...${NC}\n"
+echo -e "${BLU}ezClaps${NC} - ${AMB}Installing Python3 & PIP3...${NC}"
 sudo apt update
 sudo apt install python3 python3-pip -y
-echo -e "\n${BLU}ezClaps${NC} - ${GRN}Python3 & PIP3 installed!${NC}\n"
+echo -e "${BLU}ezClaps${NC} - ${GRN}Python3 & PIP3 installed!${NC}"
 
 #Install apt-select and then set mirror
-echo -e "\n${BLU}ezClaps${NC} - ${AMB}Updating APT mirror...${NC}\n"
+echo -e "${BLU}ezClaps${NC} - ${AMB}Updating APT mirror...${NC}"
 sudo pip3 install apt-select
 apt-select -C AU
 sudo cp /etc/apt/sources.list /etc/apt/sources.list.backup
 sudo mv sources.list /etc/apt/
-echo -e "\n${BLU}ezClaps${NC} - ${GRN}APT mirror updated!${NC}\n"
+echo -e "${BLU}ezClaps${NC} - ${GRN}APT mirror updated!${NC}"
 
 #Update to pacakge list for new mirror and upgrade system
-echo -e "\n${BLU}ezClaps${NC} - ${AMB}Upgrading system...${NC}\n"
+echo -e "${BLU}ezClaps${NC} - ${AMB}Upgrading system...${NC}"
 sudo apt update
 sudo apt upgrade -y
-echo -e "\n${BLU}ezClaps${NC} - ${GRN}System upgraded!${NC}\n"
-
-#Install Python2 & PIP2
-echo -e "\n${BLU}ezClaps${NC} - ${AMB}Installing Python2 & PIP2...${NC}\n"
-sudo apt install python2 python-dev -y
-cd /tmp
-curl https://bootstrap.pypa.io/get-pip.py --output get-pip.py
-sudo python2 get-pip.py
-echo -e "\n${BLU}ezClaps${NC} - ${GRN}Python2 & PIP2 installed!${NC}\n"
-
-#Install NodeJS & NPM
-echo -e "\n${BLU}ezClaps${NC} - ${AMB}Installing NodeJS & NPM...${NC}\n"
-curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
-sudo apt install -y nodejs
-echo -e "\n${BLU}ezClaps${NC} - ${GRN}NodeJS & NPM installed!${NC}\n"
+echo -e "${BLU}ezClaps${NC} - ${GRN}System upgraded!${NC}"
 
 #Install APT packages
-echo -e "\n${BLU}ezClaps${NC} - ${AMB}Installing APT packages...${NC}\n"
+echo -e "${BLU}ezClaps${NC} - ${AMB}Installing APT packages...${NC}"
 while read line; do
         if [[ ${line:0:1} != \# && $line != "" ]]; then
                 sudo DEBIAN_FRONTEND=noninteractive apt -y install $line
         fi
 done < apt_packages.txt
-echo -e "\n${BLU}ezClaps${NC} - ${GRN}APT packages installed!${NC}\n"
+echo -e "${BLU}ezClaps${NC} - ${GRN}APT packages installed!${NC}"
+
+#Install Python2 & PIP2
+echo -e "${BLU}ezClaps${NC} - ${AMB}Installing Python2 & PIP2...${NC}"
+sudo apt install python2 python-dev -y
+cd /tmp
+curl https://bootstrap.pypa.io/get-pip.py --output get-pip.py
+sudo python2 get-pip.py
+echo -e "${BLU}ezClaps${NC} - ${GRN}Python2 & PIP2 installed!${NC}"
+
+#Install NodeJS & NPM
+echo -e "${BLU}ezClaps${NC} - ${AMB}Installing NodeJS & NPM...${NC}"
+curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
+sudo apt install -y nodejs
+echo -e "${BLU}ezClaps${NC} - ${GRN}NodeJS & NPM installed!${NC}"
 
 #Run all build scripts
-echo -e "\n${BLU}ezClaps${NC} - ${AMB}Running build scripts...${NC}\n"
+echo -e "${BLU}ezClaps${NC} - ${AMB}Running build scripts...${NC}"
 find modules/build -iname 'install.sh' -exec bash "{}" \;
-echo -e "\n${BLU}ezClaps${NC} - ${GRN}All build scripts run!${NC}\n"
+echo -e "${BLU}ezClaps${NC} - ${GRN}All build scripts run!${NC}"
 
 #Final message
-echo -e "\n${BLU}ezClaps${NC} - ${GRN}Setup Complete!${NC}\n"
+echo -e "${BLU}ezClaps${NC} - ${GRN}Setup Complete!${NC}"
